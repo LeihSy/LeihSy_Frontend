03c6952ce86fd7ba8e2e37dd47936c64ce6f475a
Yannick Jacobs <yajasg00@hs-esslingen.de>
2025-11-15 13:00:22 +0100

    feat: implement item management features with CRUD operations and role-based access control

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,7 @@ import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- src/app/pages/items-list/items-list.ts
+++ src/app/pages/items-list/items-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-items-list',
+  standalone: true,
   imports: [],
   templateUrl: './items-list.html',
   styleUrl: './items-list.scss'
